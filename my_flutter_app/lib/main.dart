54f31444bce0f12574d51b77f642f3a3817f00de
photo2story <photo2story@naver.com>
2024-06-27 11:14:25 +0900

    Discord

---
+++ my_flutter_app/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:dotenv/dotenv.dart' as dotenv;
+import 'package:discord_logger/discord_logger.dart';
+
+void main() {
+  dotenv.load();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Load Discord credentials from environment variables
+    final discordCredentials = {
+      'channel_id': dotenv.env['DISCORD_CHANNEL_ID'],
+      'token': dotenv.env['DISCORD_APPLICATION_TOKEN'],
+    };
+
+    // Initialize DiscordLogger
+    DiscordLogger(
+      channelId: discordCredentials['channel_id'] ?? '',
+      botToken: discordCredentials['token'] ?? '',
+    );
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _sendDiscordMessage() {
+    final discord = DiscordLogger.instance;
+    discord.sendMessage("This is a test message from Flutter");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Press the button to send a message to Discord:',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendDiscordMessage,
+        tooltip: 'Send Message',
+        child: const Icon(Icons.send),
+      ), 
+    );
+  }
+}
