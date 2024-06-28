d061f0581fddf633273685bd69c552c541cc981e
github.com:photo2story <photo2story@naver.com>
2024-06-29 04:11:08 +0900

    Add process termination handling

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-# app.py 
-from flask import Flask, send_from_directory, render_template, request, jsonify
-from flask_cors import CORS
 import os
+import certifi
+import logging
+import threading
+import nest_asyncio
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from dotenv import load_dotenv
-import discord
 from discord.ext import commands
 import asyncio
 import requests
 import sys
-import certifi
-import threading
-import logging
-import nest_asyncio
 
+# 로그 설정
 logging.basicConfig(level=logging.INFO)
 
 sys.stdout.reconfigure(encoding='utf-8')
 sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flask-app'))
 
+# 필요한 모듈 임포트
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -28,14 +28,13 @@ from get_compare_stock_data import merge_csv_files, load_sector_info
 from Results_plot_mpl import plot_results_mpl
 from get_ticker import get_ticker_from_korean_name
 
+# 환경 변수 로드
 os.environ['SSL_CERT_FILE'] = certifi.where()
 load_dotenv()
-print(os.getenv('DISCORD_WEBHOOK_URL'))
-print(os.getenv('DISCORD_APPLICATION_TOKEN'))
-print(os.getenv('DISCORD_CHANNEL_ID'))
 
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
+# Flask 애플리케이션 설정
 app = Flask(__name__)
 CORS(app)
 
@@ -43,15 +42,7 @@ CORS(app)
 def index():
     return "API is running", 200
 
-sent_messages = {}
-
-def reset_sent_messages():
-    global sent_messages
-    sent_messages = {}
-    threading.Timer(10.0, reset_sent_messages).start()
-
-reset_sent_messages()
-
+# Discord 봇 설정
 TOKEN = os.getenv('DISCORD_APPLICATION_TOKEN')
 CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 
@@ -59,15 +50,14 @@ intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='', intents=intents)
 
+# 데이터 설정
 stocks = ['QQQ', 'NVDA', 'BAC', 'COIN']
 start_date = "2022-01-01"
 end_date = datetime.today().strftime('%Y-%m-%d')
 initial_investment = 30000000
 monthly_investment = 1000000
 
-processed_message_ids = set()
-login_once_flag = False  # 로그인 중복을 방지하기 위한 플래그
-
+# 함수 정의
 async def backtest_and_send(ctx, stock, option_strategy):
     total_account_balance, total_rate, str_strategy, invested_amount, str_last_signal, min_stock_data_date, file_path, result_df = estimate_stock(
         stock, start_date, end_date, initial_investment, monthly_investment, option_strategy)
@@ -78,7 +68,6 @@ async def backtest_and_send(ctx, stock, option_strategy):
     user_stock_file_path2 = file_path
 
     name = get_ticker_name(stock)
-    DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
     message = {
         'content': f"Stock: {stock} ({name})\n"
                    f"Total_rate: {total_rate:,.0f} %\n"
@@ -88,7 +77,8 @@ async def backtest_and_send(ctx, stock, option_strategy):
     }
     response = requests.post(DISCORD_WEBHOOK_URL, json=message)
     if response.status_code != 204:
-        print('Failed to send Discord message')
+        print(f'Failed to send Discord message: {response.status_code}')
+        print(f'Response content: {response.content}')
     else:
         print('Successfully sent Discord message')
 
@@ -105,7 +95,6 @@ async def buddy(ctx):
 
 @bot.command()
 async def ticker(ctx, *, query: str = None):
-    print(f'Command received: ticker with query: {query}')
     if query is None:
         await ctx.send("Please enter ticker stock name or ticker.")
         return
@@ -131,7 +120,6 @@ async def ticker(ctx, *, query: str = None):
 
     for message in response_messages:
         await ctx.send(message)
-    print(f'Sent messages for query: {query}')
 
 @bot.command()
 async def stock(ctx, *args):
@@ -160,23 +148,18 @@ async def show_all(ctx):
         await ctx.send("All results have been successfully displayed.")
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
-        print(f"Error: {e}")
 
 @bot.event
 async def on_ready():
-    global login_once_flag
-    if not login_once_flag:
-        login_once_flag = True
-        print(f'Logged in as {bot.user.name}')
-        channel = bot.get_channel(int(CHANNEL_ID))
-        if channel:
-            await channel.send(f'Bot has successfully logged in: {bot.user.name}')
+    print(f'Logged in as {bot.user.name}')
+    channel = bot.get_channel(int(CHANNEL_ID))
+    if channel:
+        await channel.send(f'Bot has successfully logged in: {bot.user.name}')
 
 @bot.command()
 async def ping(ctx):
-    if ctx.message.id not in processed_message_ids:
-        processed_message_ids.add(ctx.message.id)
-        await ctx.send(f'pong: {bot.user.name}')
+    await ctx.send(f'pong: {bot.user.name}')
+
 @app.route('/send_discord_message', methods=['POST'])
 def send_discord_message():
     data = request.json
@@ -199,7 +182,7 @@ def check_env():
     channel_id = os.getenv('DISCORD_CHANNEL_ID')
     return f"DISCORD_WEBHOOK_URL: {webhook_url}<br>DISCORD_APPLICATION_TOKEN: {token}<br>DISCORD_CHANNEL_ID: {channel_id}"
 
-
+# 비동기 루프 설정
 if __name__ == '__main__':
     nest_asyncio.apply()
     
@@ -215,4 +198,3 @@ if __name__ == '__main__':
     flask_thread.start()
 
     loop.run_until_complete(run())
-    
