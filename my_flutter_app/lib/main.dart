be993a80912cccfc3b250872c80061e25a7214bc
github.com:photo2story <photo2story@naver.com>
2024-06-28 22:25:41 +0900

    ping

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+void main() {
   runApp(MyApp());
 }
 
@@ -16,88 +14,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _isLoading = false;
-  String _message = '';
+  String _response = '';
 
-  Future<void> sendPingCommand() async {
+  Future<void> sendPing() async {
+    final response = await http.post(
+      Uri.parse('http://127.0.0.1:5000/execute_discord_command'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'message': 'ping',
+      }),
+    );
+
     setState(() {
-      _isLoading = true;
-      _message = '';
+      _response = response.body;
     });
-
-    try {
-      final url = Uri.parse('http://${dotenv.env['FLASK_SERVER_HOST']}/execute_discord_command');
-      final response = await http.post(
-        url,
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'command': 'ping',
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        setState(() {
-          _message = 'Message sent to Discord: ping';
-        });
-      } else {
-        setState(() {
-          _message = 'Failed to send message to Discord. Status code: ${response.statusCode}';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _message = 'Error: $e';
-      });
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Flutter Demo Home Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Press the button to send a message to Discord:',
+              'Response: $_response',
             ),
-            if (_isLoading)
-              CircularProgressIndicator()
-            else
-              Text(
-                _message,
-                style: TextStyle(color: _message.contains('Failed') ? Colors.red : Colors.green),
-              ),
+            ElevatedButton(
+              onPressed: sendPing,
+              child: Text('Send Ping to Discord'),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: sendPingCommand,
-        tooltip: 'Send Ping',
-        child: Icon(Icons.send),
       ),
     );
   }
