3672e0227ce20520c88c3a32cb757da15b510e01
github.com:photo2story <photo2story@naver.com>
2024-06-28 23:48:50 +0900

    png

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -35,12 +35,10 @@ class _MyHomePageState extends State<MyHomePage> {
       final response = await http.get(Uri.parse(apiUrl));
       if (response.statusCode == 200) {
         final List<dynamic> files = json.decode(response.body);
-        print('GitHub API Response: $files'); // 파일 목록을 로그에 출력
         final pngFiles = files.where((file) => file['name'].endsWith('.png')).toList();
         if (pngFiles.isNotEmpty) {
           final firstPngFile = pngFiles[0];
           final imageUrl = firstPngFile['download_url'];
-          print('Image URL: $imageUrl'); // 생성된 이미지 URL을 로그에 출력
           setState(() {
             _imageUrl = imageUrl;
             _message = '';
