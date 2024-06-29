dafdc53d278a3989bfec3336d153d01e5288da11
github.com:photo2story <photo2story@naver.com>
2024-06-29 21:41:19 +0900

    Simplified app.py for Discord ping-pong test

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-# app.py
-from flask import Flask, request, jsonify, send_from_directory, render_template
-from flask_cors import CORS
+from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import asyncio
 import nest_asyncio
-import requests
-import sys
 import threading
 import logging
 import certifi
@@ -16,29 +12,12 @@ import certifi
 # 로그 설정
 logging.basicConfig(level=logging.INFO)
 
-sys.stdout.reconfigure(encoding='utf-8')
-sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flask-app'))
-
-# 필요한 모듈 임포트
-from datetime import datetime
-import pandas as pd
-import numpy as np
-from get_ticker import load_tickers, search_tickers, get_ticker_name, update_stock_market_csv
-from estimate_stock import estimate_snp, estimate_stock
-from Results_plot import plot_comparison_results, plot_results_all
-from get_compare_stock_data import merge_csv_files, load_sector_info
-from Results_plot_mpl import plot_results_mpl
-from get_ticker import get_ticker_from_korean_name
-
 # 환경 변수 로드
 os.environ['SSL_CERT_FILE'] = certifi.where()
 load_dotenv()
 
-DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
-
 # Flask 애플리케이션 설정
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
@@ -50,106 +29,11 @@ CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 
 intents = discord.Intents.all()
 intents.message_content = True
-bot = commands.Bot(command_prefix='', intents=intents)
-
-# 데이터 설정
-stocks = ['QQQ', 'NVDA', 'BAC', 'COIN']
-start_date = "2022-01-01"
-end_date = datetime.today().strftime('%Y-%m-%d')
-initial_investment = 30000000
-monthly_investment = 1000000
-
-# 함수 정의
-async def backtest_and_send(ctx, stock, option_strategy):
-    total_account_balance, total_rate, str_strategy, invested_amount, str_last_signal, min_stock_data_date, file_path, result_df = estimate_stock(
-        stock, start_date, end_date, initial_investment, monthly_investment, option_strategy)
-    min_stock_data_date = str(min_stock_data_date).split(' ')[0]
-    user_stock_file_path1 = file_path
-
-    file_path = estimate_snp(stock, 'VOO', min_stock_data_date, end_date, initial_investment, monthly_investment, option_strategy, result_df)
-    user_stock_file_path2 = file_path
-
-    name = get_ticker_name(stock)
-    message = {
-        'content': f"Stock: {stock} ({name})\n"
-                   f"Total_rate: {total_rate:,.0f} %\n"
-                   f"Invested_amount: {invested_amount:,.0f} $\n"
-                   f"Total_account_balance: {total_account_balance:,.0f} $\n"
-                   f"Last_signal: {str_last_signal} \n"
-    }
-    response = requests.post(DISCORD_WEBHOOK_URL, json=message)
-    if response.status_code != 204:
-        print(f'Failed to send Discord message: {response.status_code}')
-        print(f'Response content: {response.content}')
-    else:
-        print('Successfully sent Discord message')
-
-    plot_comparison_results(user_stock_file_path1, user_stock_file_path2, stock, 'VOO', total_account_balance, total_rate, str_strategy, invested_amount, min_stock_data_date)
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Waiting"))
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
-async def buddy(ctx):
-    for stock in stocks:
-        await backtest_and_send(ctx, stock, 'modified_monthly')
-        plot_results_mpl(stock, start_date, end_date)
-        await asyncio.sleep(2)
-    await ctx.send("Backtesting results have been organized.")
-
-@bot.command()
-async def ticker(ctx, *, query: str = None):
-    if query is None:
-        await ctx.send("Please enter ticker stock name or ticker.")
-        return
-
-    ticker_dict = load_tickers()
-    matching_tickers = search_tickers(query, ticker_dict)
-
-    if not matching_tickers:
-        await ctx.send("No search results.")
-        return
-
-    response_message = "Search results:\n"
-    response_messages = []
-    for symbol, name in matching_tickers:
-        line = f"{symbol} - {name}\n"
-        if len(response_message) + len(line) > 2000:
-            response_messages.append(response_message)
-            response_message = "Search results (continued):\n"
-        response_message += line
-
-    if response_message:
-        response_messages.append(response_message)
-
-    for message in response_messages:
-        await ctx.send(message)
-
-@bot.command()
-async def stock(ctx, *args):
-    stock_name = ' '.join(args)
-    await ctx.send(f'Arguments passed by command: {stock_name}')
-    try:
-        info_stock = str(stock_name).upper()
-        if info_stock.startswith('K '):
-            korean_stock_name = info_stock[2:].upper()
-            korean_stock_code = get_ticker_from_korean_name(korean_stock_name)
-            if korean_stock_code is None:
-                await ctx.send(f'Cannot find the stock {korean_stock_name}.')
-                return
-            else:
-                info_stock = korean_stock_code
-
-        await backtest_and_send(ctx, info_stock, option_strategy='1')
-        plot_results_mpl(info_stock, start_date, end_date)
-    except Exception as e:
-        await ctx.send(f'An error occurred: {e}')
-
-@bot.command()
-async def show_all(ctx):
-    try:
-        await plot_results_all()
-        await ctx.send("All results have been successfully displayed.")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
+async def ping(ctx):
+    await ctx.send(f'pong: {bot.user.name}')
 
 @bot.event
 async def on_ready():
@@ -157,32 +41,6 @@ async def on_ready():
     channel = bot.get_channel(int(CHANNEL_ID))
     if channel:
         await channel.send(f'Bot has successfully logged in: {bot.user.name}')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'pong: {bot.user.name}')
-
-@app.route('/send_discord_message', methods=['POST'])
-def send_discord_message():
-    data = request.json
-    message = data.get('message')
-    if message:
-        payload = {
-            'content': message
-        }
-        response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
-        if response.status_code == 204:
-            return jsonify({'status': 'success'}), 200
-        else:
-            return jsonify({'status': 'failure', 'error': response.text}), response.status_code
-    return jsonify({'status': 'failure', 'error': 'No message provided'}), 400
-
-@app.route('/check_env')
-def check_env():
-    webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
-    token = os.getenv('DISCORD_APPLICATION_TOKEN')
-    channel_id = os.getenv('DISCORD_CHANNEL_ID')
-    return f"DISCORD_WEBHOOK_URL: {webhook_url}<br>DISCORD_APPLICATION_TOKEN: {token}<br>DISCORD_CHANNEL_ID: {channel_id}"
 
 # 비동기 루프 설정
 if __name__ == '__main__':
