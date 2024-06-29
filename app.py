4d74ee574cd7ec2b16e05592af873f244d6566f4
github.com:photo2story <photo2story@naver.com>
2024-06-29 22:58:41 +0900

    Update Gunicorn settings and ensure proper shutdown

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import sys
 import asyncio
 from datetime import datetime
-import matplotlib.pyplot as plt
 from flask import Flask
 
 # 현재 디렉토리 경로를 시스템 경로에 추가
@@ -11,8 +10,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flask-app'))
 # 필요한 모듈을 가져오기
 from Data_export import backtest_and_send
 from Results_plot_mpl import plot_results_mpl
+from get_ticker import load_tickers, search_tickers, get_ticker_name, get_ticker_from_korean_name
+from estimate_stock import estimate_snp, estimate_stock
+from Results_plot import plot_comparison_results
+from get_compare_stock_data import merge_csv_files, load_sector_info
 
 app = Flask(__name__)
+
+stocks = ['QQQ', 'NVDA', 'BAC', 'COIN']  # 주식 종목 리스트
+start_date = "2022-01-01"  # 시작 날짜
+end_date = datetime.today().strftime('%Y-%m-%d')  # 종료 날짜
+initial_investment = 30000000  # 초기 투자금
+monthly_investment = 1000000  # 월별 투자금
 
 @app.route('/')
 def home():
@@ -20,16 +29,12 @@ def home():
 
 @app.route('/run-backtest')
 def run_backtest():
-    stocks = ['AAPL', 'MSFT', 'GOOGL']  # 예시로 몇 개의 주식 종목 설정
-    start_date = '2020-01-01'
-    end_date = datetime.now().strftime('%Y-%m-%d')
-
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     async def main():
         for stock in stocks:
-            await backtest_and_send(stock, 'modified_monthly')
+            await backtest_and_send(stock, 'modified_monthly', start_date, end_date, initial_investment, monthly_investment)
             plot_results_mpl(stock, start_date, end_date)
             await asyncio.sleep(2)
 
