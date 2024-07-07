0da7fd2f9e507028f07ba25859c063e31d0808d5
github.com:photo2story <photo2story@naver.com>
2024-07-08 00:47:35 +0900

    import

--- app1.py
+++ app1.py
@@ -3,7 +3,35 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import tasks, commands
+import logging
+# import threading
+# import asyncio
+from flask import Flask, render_template, send_from_directory, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+# from discord.ext import commands
+# import discord
+import requests
+import certifi
+import sys
+# import nest_asyncio
+import git
 
+logging.basicConfig(level=logging.INFO)  # Set logging level to INFO
+sys.stdout.reconfigure(encoding='utf-8') # 한글 출력을 위한 인코딩 설정
+sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flutter-app'))
+
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from get_ticker import load_tickers, search_tickers, get_ticker_name, update_stock_market_csv
+from estimate_stock import estimate_snp, estimate_stock
+from Results_plot import plot_comparison_results, plot_results_all
+from get_compare_stock_data import merge_csv_files, load_sector_info
+from Results_plot_mpl import plot_results_mpl
+from get_ticker import get_ticker_from_korean_name
+from git_operations import move_files_to_images_folder
+from github_operations import save_csv_to_github, save_image_to_github, is_valid_stock, ticker_path
 # Load environment variables from .env file
 load_dotenv()
 
