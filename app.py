f3112b2fef955d8a4806f8953fba5d412b4da500
github.com:photo2story <photo2story@naver.com>
2024-07-08 22:32:16 +0900

    5000

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def execute_stock_command():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=5000)
 
 
 
