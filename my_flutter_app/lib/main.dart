f4aa723f82a91d66072756e2a891deb8ca230dd3
github.com:photo2story <photo2story@naver.com>
2024-07-09 03:41:40 +0900

    dart env

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:photo_view/photo_view.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';  // flutter_dotenv 패키지 추가
 
 void main() {
   runApp(MyApp());
@@ -36,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final String apiUrl = 'http://192.168.0.5:5000/api/get_reviewed_tickers';
   final String descriptionApiUrl = 'http://192.168.0.5:5000/generate_description';
   final String executeCommandApiUrl = 'http://192.168.0.5:5000/execute_stock_command';
-  final String discordWebhookUrl = 'YOUR_DISCORD_WEBHOOK_URL';  // Add your Discord webhook URL here
+  final String discordWebhookUrl = dotenv.env['DISCORD_WEBHOOK_URL']!;  // 환경 변수
 
   Future<void> fetchReviewedTickers() async {
     try {
@@ -54,6 +55,48 @@ class _MyHomePageState extends State<MyHomePage> {
     } catch (e) {
       setState(() {
         _message = 'Error occurred while fetching tickers: $e';
+      });
+    }
+  }
+
+  Future<void> fetchImages(String stockTicker) async {
+    try {
+      final response = await http.get(Uri.parse('https://api.github.com/repos/photo2story/my-flutter-app/contents/static/images'));
+      if (response.statusCode == 200) {
+        final List<dynamic> files = json.decode(response.body);
+        final comparisonFile = files.firstWhere(
+            (file) => file['name'] == 'comparison_${stockTicker}_VOO.png',
+            orElse: () => null);
+        final resultFile = files.firstWhere(
+            (file) => file['name'] == 'result_mpl_${stockTicker}.png',
+            orElse: () => null);
+
+        if (comparisonFile != null && resultFile != null) {
+          setState(() {
+            _comparisonImageUrl = comparisonFile['download_url'];
+            _resultImageUrl = resultFile['download_url'];
+            _message = '';
+          });
+          await generateDescription(stockTicker); // 그래프 설명 생성
+        } else {
+          setState(() {
+            _comparisonImageUrl = '';
+            _resultImageUrl = '';
+            _message = '해당 주식 티커에 대한 이미지를 찾을 수 없습니다';
+          });
+        }
+      } else {
+        setState(() {
+          _comparisonImageUrl = '';
+          _resultImageUrl = '';
+          _message = 'GitHub API 호출 실패: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _comparisonImageUrl = '';
+        _resultImageUrl = '';
+        _message = '오류 발생: $e';
       });
     }
   }
