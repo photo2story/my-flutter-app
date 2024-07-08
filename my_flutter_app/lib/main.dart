fe4ba85860e5a88d442d986497b24e8c166453a6
github.com:photo2story <photo2story@naver.com>
2024-07-09 01:19:43 +0900

    dart

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -26,105 +26,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _comparisonImageUrl = '';
-  String _resultImageUrl = '';
   String _message = '';
-  String _description = '';
-  List<String> _tickers = [];
   final TextEditingController _controller = TextEditingController();
 
-  // 환경 변수 직접 포함
-  final String apiUrl = 'http://192.168.0.5:5000/api/get_reviewed_tickers';
-  final String descriptionApiUrl = 'http://192.168.0.5:5000/generate_description';
   final String executeCommandApiUrl = 'http://192.168.0.5:5000/execute_stock_command';
-
-  Future<void> fetchReviewedTickers() async {
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-      if (response.statusCode == 200) {
-        final List<dynamic> tickers = json.decode(response.body);
-        setState(() {
-          _tickers = List<String>.from(tickers);
-        });
-      } else {
-        setState(() {
-          _message = 'Error occurred while fetching tickers: ${response.statusCode}';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _message = 'Error occurred while fetching tickers: $e';
-      });
-    }
-  }
-
-  Future<void> fetchImages(String stockTicker) async {
-    try {
-      final response = await http.get(Uri.parse('https://api.github.com/repos/photo2story/my-flutter-app/contents/static/images'));
-      if (response.statusCode == 200) {
-        final List<dynamic> files = json.decode(response.body);
-        final comparisonFile = files.firstWhere(
-            (file) => file['name'] == 'comparison_${stockTicker}_VOO.png',
-            orElse: () => null);
-        final resultFile = files.firstWhere(
-            (file) => file['name'] == 'result_mpl_${stockTicker}.png',
-            orElse: () => null);
-
-        if (comparisonFile != null && resultFile != null) {
-          setState(() {
-            _comparisonImageUrl = comparisonFile['download_url'];
-            _resultImageUrl = resultFile['download_url'];
-            _message = '';
-          });
-          await generateDescription(stockTicker); // 그래프 설명 생성
-        } else {
-          setState(() {
-            _comparisonImageUrl = '';
-            _resultImageUrl = '';
-            _message = '해당 주식 티커에 대한 이미지를 찾을 수 없습니다';
-          });
-        }
-      } else {
-        setState(() {
-          _comparisonImageUrl = '';
-          _resultImageUrl = '';
-          _message = 'GitHub API 호출 실패: ${response.statusCode}';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _comparisonImageUrl = '';
-        _resultImageUrl = '';
-        _message = '오류 발생: $e';
-      });
-    }
-  }
-
-  Future<void> generateDescription(String stockTicker) async {
-    try {
-      final response = await http.post(
-        Uri.parse(descriptionApiUrl),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'stock_ticker': stockTicker}),
-      );
-
-      if (response.statusCode == 200) {
-        final responseBody = json.decode(response.body);
-        final description = responseBody['description'];
-        setState(() {
-          _description = description;
-        });
-      } else {
-        setState(() {
-          _description = '설명 생성 실패: ${response.statusCode}';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _description = '오류 발생: $e';
-      });
-    }
-  }
 
   Future<void> executeStockCommand(String stockTicker) async {
     try {
@@ -141,29 +46,14 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       } else {
         setState(() {
-          _message = '명령 실행 실패: ${response.statusCode}';
+          _message = 'Command execution failed: ${response.statusCode}';
         });
       }
     } catch (e) {
       setState(() {
-        _message = '오류 발생: $e';
+        _message = 'An error occurred: $e';
       });
     }
-  }
-
-  void _openImage(BuildContext context, String imageUrl) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ImageScreen(imageUrl: imageUrl),
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchReviewedTickers();
   }
 
   @override
@@ -186,14 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     border: OutlineInputBorder(),
                     labelText: 'Enter Stock Ticker',
                   ),
-                  onChanged: (value) {
-                    _controller.value = TextEditingValue(
-                      text: value.toUpperCase(),
-                      selection: _controller.selection,
-                    );
-                  },
                   onSubmitted: (value) {
-                    fetchImages(_controller.text.toUpperCase());
+                    executeStockCommand(_controller.text.toUpperCase());
                   },
                 ),
               ),
@@ -204,92 +88,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Text('Test Stock'),
               ),
               SizedBox(height: 20),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Reviewed Stocks:',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Wrap(
-                children: _tickers.map((ticker) {
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        fetchImages(ticker);
-                      },
-                      child: Text(
-                        ticker,
-                        style: TextStyle(fontSize: 14, color: Colors.blue),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 20),
-              _comparisonImageUrl.isNotEmpty
-                  ? GestureDetector(
-                      onTap: () => _openImage(context, _comparisonImageUrl),
-                      child: Image.network(
-                        _comparisonImageUrl,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Text('Failed to load comparison image');
-                        },
-                      ),
-                    )
-                  : Container(),
-              SizedBox(height: 20),
-              _resultImageUrl.isNotEmpty
-                  ? GestureDetector(
-                      onTap: () => _openImage(context, _resultImageUrl),
-                      child: Image.network(
-                        _resultImageUrl,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Text('Failed to load result image');
-                        },
-                      ),
-                    )
-                  : Container(),
-              SizedBox(height: 20),
               _message.isNotEmpty
                   ? Text(
                       _message,
                       style: TextStyle(fontSize: 16, color: Colors.red),
                     )
                   : Container(),
-              SizedBox(height: 20),
-              _description.isNotEmpty
-                  ? Text(
-                      _description,
-                      style: TextStyle(fontSize: 16, color: Colors.green),
-                    )
-                  : Container(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class ImageScreen extends StatelessWidget {
-  final String imageUrl;
-
-  ImageScreen({required this.imageUrl});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Preview'),
-      ),
-      body: Center(
-        child: PhotoView(
-          imageProvider: NetworkImage(imageUrl),
-          errorBuilder: (context, error, stackTrace) {
-            return Text('Failed to load image');
-          },
         ),
       ),
     );
