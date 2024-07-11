123811c457510a300b4e89e45bbd75972cdfb59a
github.com:photo2story <photo2story@naver.com>
2024-07-11 21:08:29 +0900

    bot

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, send_from_directory, jsonify, request
 from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
 
-# 현재 디렉토리를 sys.path에 추가
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# # 현재 디렉토리를 sys.path에 추가
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # Add my-flutter-app directory to sys.path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flutter-app'))
 
