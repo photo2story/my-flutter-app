62b2b2d15860577f57fedabf27eb335b4bf1bd4b
github.com:photo2story <photo2story@naver.com>
2024-07-11 23:24:59 +0900

    bot

--- bot.py
+++ bot.py
@@ -7,12 +7,11 @@ import discord
 from discord.ext import tasks, commands
 
 # Add my-flutter-app directory to sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flutter-app'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'my-flask-app')))
 
 # 사용자 정의 모듈 임포트
 from git_operations import move_files_to_images_folder
-from get_ticker import load_tickers, search_tickers, get_ticker_name, update_stock_market_csv
-from get_ticker import get_ticker_from_korean_name
+from get_ticker import load_tickers, search_tickers, get_ticker_name, update_stock_market_csv, get_ticker_from_korean_name
 from estimate_stock import estimate_stock
 from Results_plot_mpl import plot_results_mpl
 from github_operations import is_valid_stock, ticker_path
@@ -125,4 +124,3 @@ async def run_bot():
 
 if __name__ == '__main__':
     asyncio.run(run_bot())
-
