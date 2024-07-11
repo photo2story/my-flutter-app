62cc54b3029d5379bb9212784df4ac955ab953ba
github.com:photo2story <photo2story@naver.com>
2024-07-11 20:48:56 +0900

    bot

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'my-flutter-app'))
 
 # 사용자 정의 모듈 임포트
-from github_operations import move_files_to_images_folder
+from git_operations import move_files_to_images_folder
 
 load_dotenv()
 
