afa1623ffc2caf6cb829b756a4be638e1abba029
waterlee.kr <waterlee.kr@gmail.com>
2024-07-13 12:18:51 +0000

    merge

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ from backtest_send import backtest_and_send
 
 # get_account_balance 모듈 임포트
 from get_account_balance import get_balance, get_ticker_price, get_market_from_ticker
-from get_compare_stock_data import load_sector_info, merge_csv_files  # 추가된 모듈
+from get_compare_stock_data import load_sector_info, merge_csv_files
 
 load_dotenv()
 
