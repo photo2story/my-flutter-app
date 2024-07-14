1c854a9ecde097057263a59000af7506e2291908
github.com:photo2story <photo2story@naver.com>
2024-07-15 01:09:48 +0900

    backup

---
+++ bot copy.py
@@ -0,0 +1,210 @@
+# bot.py
+import os
+import sys
+import asyncio
+from dotenv import load_dotenv
+import discord
+from discord.ext import tasks, commands
+import certifi
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import threading
+import config  # config.py 임포트
+
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+# Add my-flask-app directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'my-flask-app')))
+
+# 사용자 정의 모듈 임포트
+from git_operations import move_files_to_images_folder
+from get_ticker import load_tickers, search_tickers_and_respond, get_ticker_name, update_stock_market_csv, get_ticker_from_korean_name
+from estimate_stock import estimate_stock
+from Results_plot import plot_results_all
+from Results_plot_mpl import plot_results_mpl
+from github_operations import ticker_path
+from backtest_send import backtest_and_send
+from get_ticker  import is_valid_stock
+
+# get_account_balance 모듈 임포트
+from get_account_balance import get_balance, get_ticker_price, get_market_from_ticker
+from get_compare_stock_data import load_sector_info, merge_csv_files
+
+# gemini 모듈 임포트
+from gemini import analyze_with_gemini
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_APPLICATION_TOKEN')
+CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
+H_APIKEY = os.getenv('H_APIKEY')
+H_SECRET = os.getenv('H_SECRET')
+H_ACCOUNT = os.getenv('H_ACCOUNT')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='', intents=intents)
+
+processed_message_ids = set()
+
+def check_duplicate_message():
+    async def predicate(ctx):
+        if ctx.message.id in processed_message_ids:
+            return False
+        processed_message_ids.add(ctx.message.id)
+        return True
+    return commands.check(predicate)
+
+bot_started = False
+
+@bot.event
+async def on_ready():
+    global bot_started
+    if not bot_started:
+        print(f'Logged in as {bot.user.name}')
+        channel = bot.get_channel(CHANNEL_ID)
+        if channel:
+            await channel.send(f'Bot has successfully logged in as {bot.user.name}')
+        else:
+            print(f'Failed to get channel with ID {CHANNEL_ID}')
+        bot_started = True
+
+@tasks.loop(minutes=5)
+async def send_msg():
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send('Hello')
+        print(f'Message sent to channel {CHANNEL_ID}')
+    else:
+        print(f"Failed to find channel with ID: {CHANNEL_ID}")
+
+@bot.command(name='buddy')
+@check_duplicate_message()
+async def buddy(ctx):
+    loop = asyncio.get_running_loop()
+
+    for stock in config.STOCKS:
+        await backtest_and_send(ctx, stock, 'modified_monthly', bot)
+        if is_valid_stock(stock):
+            try:
+                plot_results_mpl(stock, config.START_DATE, config.END_DATE)
+            except KeyError as e:
+                await ctx.send(f"An error occurred while plotting {stock}: {e}")
+                print(f"Error plotting {stock}: {e}")
+        await asyncio.sleep(2)
+
+    print("Updating stock market CSV...")
+    await loop.run_in_executor(None, update_stock_market_csv, ticker_path, config.STOCKS)
+    
+    print("Loading sector info...")
+    sector_dict = await loop.run_in_executor(None, load_sector_info)
+    
+    print("Merging CSV files...")
+    path = '.'
+    await loop.run_in_executor(None, merge_csv_files, path, sector_dict)
+
+    await ctx.send("백테스팅 결과가 섹터별로 정리되었습니다.")
+    move_files_to_images_folder()
+
+@bot.command(name='ticker')
+@check_duplicate_message()
+async def ticker(ctx, *, query: str = None):
+    print(f'Command received: ticker with query: {query}')
+    if query is None:
+        await ctx.send("Please enter ticker stock name or ticker.")
+        return
+
+    await search_tickers_and_respond(ctx, query)
+
+@bot.command(name='stock')
+@check_duplicate_message()
+async def stock(ctx, *args):
+    stock_name = ' '.join(args)
+    await ctx.send(f'Arguments passed by command: {stock_name}')
+    try:
+        info_stock = str(stock_name).upper()
+        if info_stock.startswith('K '):
+            korean_stock_name = info_stock[2:].upper()
+            korean_stock_code = get_ticker_from_korean_name(korean_stock_name)
+            if korean_stock_code is None:
+                await ctx.send(f'Cannot find the stock {korean_stock_name}.')
+                return
+            else:
+                info_stock = korean_stock_code
+
+        await ctx.send(f'Running backtest_and_send for {info_stock}')
+        await backtest_and_send(ctx, info_stock, option_strategy='1', bot=bot)
+        
+        await ctx.send(f'Plotting results for {info_stock}')
+        plot_results_mpl(info_stock, config.START_DATE, config.END_DATE)
+        move_files_to_images_folder()
+        await ctx.send(f'Successfully processed {info_stock}')
+        
+    except Exception as e:
+        await ctx.send(f'An error occurred: {e}')
+        print(f'Error processing {info_stock}: {e}')
+
+@bot.command(name='show_all')
+@check_duplicate_message()
+async def show_all(ctx):
+    try:
+        await plot_results_all()
+        await ctx.send("All results have been successfully displayed.")
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+        print(f"Error: {e}")
+
+@bot.command(name='ping')
+@check_duplicate_message()
+async def ping(ctx):
+    await ctx.send(f'pong: {bot.user.name}')
+    print(f'Ping command received and responded with pong.')
+
+@bot.command(name='account')
+@check_duplicate_message()
+async def account(ctx, ticker: str):
+    try:
+        ticker = ticker.upper()  # 티커를 대문자로 변환
+        exchange = get_market_from_ticker(ticker)
+        last_price = get_ticker_price(H_APIKEY, H_SECRET, H_ACCOUNT, exchange, ticker)
+        await ctx.send(f'The exchange for {ticker} is {exchange}')
+        await ctx.send(f'Last price of {ticker} is {last_price}')
+    except Exception as e:
+        await ctx.send(f'An error occurred: {e}')
+        print(f'Error processing account for {ticker}: {e}')
+
+@bot.command(name='gemini')
+@check_duplicate_message()
+async def gemini(ctx, ticker: str):
+    try:
+        ticker = ticker.upper()  # 티커를 대문자로 변환
+        report = analyze_with_gemini(ticker)
+        await ctx.send(report)
+    except Exception as e:
+        await ctx.send(f'An error occurred while analyzing {ticker} with Gemini API: {e}')
+        print(f'Error processing Gemini analysis for {ticker}: {e}')
+
+async def run_bot():
+    await bot.start(TOKEN)
+
+def run_server():
+    port = int(os.environ.get('PORT', 8080))
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f'Starting server on port {port}')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    # HTTP 서버 시작
+    server_thread = threading.Thread(target=run_server, daemon=True)
+    server_thread.start()
+    
+    # 봇 실행
+    asyncio.run(run_bot())
+
+
+#  .\.venv\Scripts\activate
+# source .venv/bin/activate
+# # python bot.py    
+# docker build -t asia.gcr.io/my-flask-app-429017/bot .
+# docker push asia.gcr.io/my-flask-app-429017/bot
+# gcloud run deploy bot --image asia.gcr.io/my-flask-app-429017/bot --platform managed --region asia-northeast3 --allow-unauthenticated
--- my-flask-app/gemini copy.py
+++ my-flask-app/gemini copy.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import shutil
 import matplotlib.pyplot as plt
+from git_operations import move_files_to_images_folder
+# from googleapiclient.discovery import build
 
 # Load environment variables
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+# GOOGLE_CSE_ID = os.getenv('GOOGLE_CSE_ID')
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 GITHUB_RAW_BASE_URL = "https://raw.githubusercontent.com/photo2story/my-flutter-app/main/static/images"
 
@@ -46,18 +49,32 @@ def analyze_with_gemini(ticker):
         voo_file = f'result_VOO_{ticker}.csv'
         df_voo = pd.read_csv(voo_file)
 
-        # CSV 데이터를 문자열로 변환
-        voo_data_str = df_voo.to_csv(index=False)
+        # 데이터 추출
+        final_rate = df_voo['rate'].iloc[-1]
+        final_rate_vs = df_voo['rate_vs'].iloc[-1]
+        sma_5 = df_voo['sma05_ta'].iloc[-1]
+        sma_20 = df_voo['sma20_ta'].iloc[-1]
+        sma_60 = df_voo['sma60_ta'].iloc[-1]
+        rsi = df_voo['rsi_ta'].iloc[-1]
+        ppo = df_voo['ppo_histogram'].iloc[-1]
 
-        # Gemini API 호출을 위한 프롬프트 준비
+        # 프롬프트 준비
         prompt_voo = f"""
-        1) 제공된 자료의 수익율(rate)와 S&P 500(VOO)의 수익율(rate_vs)과 비교해서 이격된 정도를 알려줘(간단하게 자료 맨마지막날의 수익율차이: rate와 rate_vs값으로) {ticker}:\n{voo_data_str}
-        2) 제공된 자료의 최근 주가 변동(간단하게: 5일, 20일, 60일 이동평균 수치로)
-        3) 제공된 자료의 RSI, PPO 인덱스 지표를 분석해줘 (간단하게: RSI 40 과매도)
+        1) 제공된 자료의 수익율(rate)와 S&P 500(VOO)의 수익율(rate_vs)과 비교해서 이격된 정도를 알려줘 (간단하게 자료 맨마지막날의 누적수익율차이):
+           리뷰할 주식티커명 = {ticker}
+           리뷰주식의 누적수익률 = {final_rate}
+           기준이 되는 비교주식(S&P 500, VOO)의 누적수익율 = {final_rate_vs}
+        2) 제공된 자료의 최근 주가 변동(간단하게: 5일, 20일, 60일 이동평균 수치로):
+           5일이동평균 = {sma_5}
+           20일이동평균 = {sma_20}
+           60일이동평균 = {sma_60}
+        3) 제공된 자료의 RSI, PPO 인덱스 지표를 분석해줘 (간단하게):
+           RSI = {rsi}
+           PPO = {ppo}
         4) 최근 실적 및 전망(웹검색해서 간단하게: 최근 매출,영업이익, 다음분기 전망 매출, 영업이익)
-        5) 애널리스트 의견(웹검색해서 간단하게: 매수, 강력매수..)
+        5) 애널리스트 의견(웹검색해서 간단하게: 최근 애널리스트 의견.)
         6) 레포트는 ["candidates"][0]["content"]["parts"][0]["text"]의 구조의 텍스트로 만들어줘
-        7) 레포트는 영어로 만들어줘
+        7) 레포트는 한글로 만들어줘
         """
 
         # Gemini API 호출
@@ -65,7 +82,6 @@ def analyze_with_gemini(ticker):
 
         # 리포트를 텍스트로 저장
         report_text = response_ticker.text        
-        # report_text = response_ticker.result["candidates"][0]["content"]["parts"][0]["text"]
         print(report_text)
 
         # 디스코드 웹훅 메시지로 전송
@@ -78,10 +94,11 @@ def analyze_with_gemini(ticker):
         with open(report_file, 'w', encoding='utf-8') as file:
             file.write(report_text)
 
-        # 리포트를 static/images 폴더로 이동
-        destination_folder = os.path.join('static', 'images')
-        os.makedirs(destination_folder, exist_ok=True)
-        shutil.move(report_file, os.path.join(destination_folder, os.path.basename(report_file)))
+        # 리포트를 static/images 폴더로 이동 및 커밋
+        # destination_folder = os.path.join('static', 'images')
+        # os.makedirs(destination_folder, exist_ok=True)
+        # shutil.move(report_file, os.path.join(destination_folder, os.path.basename(report_file)))
+        move_files_to_images_folder()
 
         return f'Gemini Analysis for {ticker} (VOO) has been sent to Discord and saved as a text file.'
 
@@ -95,9 +112,6 @@ if __name__ == '__main__':
     ticker = 'AAPL'  # Example ticker
     report = analyze_with_gemini(ticker)
     print(report)
-
-
-
 
 """
 source .venv/bin/activate
