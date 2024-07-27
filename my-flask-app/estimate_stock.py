e02ad5bff3cf358cfbadc62af2d19bbbfb952bf9
github.com:photo2story <photo2story@naver.com>
2024-07-27 23:21:48 +0900

    Auto-commit moved files

--- my-flask-app/estimate_stock.py
+++ my-flask-app/estimate_stock.py
@@ -1,12 +1,8 @@
 # estimate_stock.py
-import requests
-from Get_data import get_stock_data
-import My_strategy
-from Data_export import export_csv
-import os, sys
-from datetime import datetime
 import pandas as pd
-from dotenv import load_dotenv
+import os,sys
+from My_strategy import my_strategy
+from Data_export import export_csv
 # 루트 디렉토리를 sys.path에 추가
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -33,7 +29,6 @@ def estimate_stock(stock, start_date, end_date, initial_investment, monthly_inve
 
     return total_account_balance, total_rate, str_strategy, invested_amount, str_last_signal, min_stock_data_date, file_path, result_df
 
-
 def is_date_range_matching(file_path, min_stock_data_date, end_date):
     """파일에 저장된 데이터의 날짜 범위가 주어진 날짜 범위와 일치하는지 확인합니다."""
     df = pd.read_csv(file_path, parse_dates=['Date'])
@@ -47,24 +42,17 @@ def estimate_snp(stock1, stock2, min_stock_data_date, end_date, initial_investme
     # VOO 퍼포먼스 데이터가 이미 존재하고 유효한지 확인
     voo_performance_data = None
     if os.path.exists(config.VOO_PERFORMANCE_FILE_PATH):
-        print(f"VOO performance file found at {config.VOO_PERFORMANCE_FILE_PATH}. Checking date range...")
         voo_performance_data = pd.read_csv(config.VOO_PERFORMANCE_FILE_PATH, index_col='Date', parse_dates=True)
         if is_date_range_matching(config.VOO_PERFORMANCE_FILE_PATH, min_stock_data_date, end_date):
-            print("VOO performance data is valid and within the required date range.")
             voo_performance_data = voo_performance_data[['rate_vs']]
         else:
-            print("Date range mismatch. Discarding existing VOO performance data.")
             voo_performance_data = None
-    else:
-        print(f"VOO performance file not found at {config.VOO_PERFORMANCE_FILE_PATH}.")
 
     # 유효한 VOO 데이터가 없으면 새로 생성
-    if voo_performance_data is None:
-        print("Generating new VOO performance data...")
+    if voo_performance_data == None:
         result_dict2 = my_strategy(stock_data, initial_investment, monthly_investment, option_strategy)
         voo_performance_data = pd.DataFrame(result_dict2['result'])
         voo_performance_data.to_csv(config.VOO_PERFORMANCE_FILE_PATH)
-        print(f"New VOO performance data generated and saved at {config.VOO_PERFORMANCE_FILE_PATH}.")
 
     # 최종 비교 데이터를 준비
     safe_ticker = stock1.replace('/', '-')
@@ -77,11 +65,8 @@ def estimate_snp(stock1, stock2, min_stock_data_date, end_date, initial_investme
     combined_df = result_df.join(result_df2['rate_vs'])
     combined_df.fillna(0, inplace=True)
     combined_df.to_csv(file_path, float_format='%.2f', index=False)
-    
-    print(f"Comparison data saved to {file_path}")
 
     return file_path
-
 
 
 # 테스트 코드
