7b5e9a980bac73fc1f3ad94740d833101bce2962
github.com:photo2story <photo2story@naver.com>
2024-07-27 13:15:13 +0900

    Auto-commit moved files

--- my-flask-app/My_strategy.py
+++ my-flask-app/My_strategy.py
@@ -135,7 +135,7 @@ def my_strategy(stock_data, initial_investment, monthly_investment, option_strat
             # Hold cash
             pass
 
-        sell_result = strategy_sell(current_date,rsi_ta, PPO_SELL, Sudden_fall, option_strategy)
+        sell_result = strategy_sell(current_date, stock_ticker, rsi_ta, PPO_SELL, Sudden_fall, option_strategy)
 
         if isinstance(sell_result, tuple):
             ta_sell_amount, sell_signal, Sudden_fall = sell_result
--- my-flask-app/Strategy_sell.py
+++ my-flask-app/Strategy_sell.py
@@ -1,7 +1,7 @@
 # strategy_sell.py
 from datetime import datetime
 
-def strategy_sell(date_str, rsi_ta, PPO_SELL,Sudden_fall, option_strategy):
+def strategy_sell(date_str, stock_ticker, rsi_ta, PPO_SELL,Sudden_fall, option_strategy):
     # 날짜 문자열을 datetime 객체로 변환
     date = date_str
 
