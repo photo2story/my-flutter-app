a56cce163b96ff5d7e04d1a4d1e477a1f4c85f1a
github.com:photo2story <photo2story@naver.com>
2024-07-28 13:27:55 +0900

    Auto-commit moved files

--- my-flask-app/backtest_send.py
+++ my-flask-app/backtest_send.py
@@ -38,24 +38,11 @@ async def backtest_and_send(ctx, stock, option_strategy='1', bot=None):
         file_path = estimate_snp(stock, 'VOO', min_stock_data_date, config.END_DATE, config.INITIAL_INVESTMENT, config.MONTHLY_INVESTMENT, option_strategy, result_df)
         user_stock_file_path2 = file_path
 
-        name = get_ticker_name(stock)
-        message = {
-            'content': f"Stock: {stock} ({name})\n"
-                    f"Total_rate: {total_rate:,.0f} %\n"
-                    f"Invested_amount: {invested_amount:,.0f} $\n"
-                    f"Total_account_balance: {total_account_balance:,.0f} $\n"
-                    f"Last_signal: {str_last_signal} \n"
-        }
-        response = requests.post(config.DISCORD_WEBHOOK_URL, json=message)
-        if response.status_code != 204:
-            print('Failed to send Discord message')
-        else:
-            print('Successfully sent Discord message')
-
         plot_comparison_results(user_stock_file_path1, user_stock_file_path2, stock, 'VOO', total_account_balance, total_rate, str_strategy, invested_amount, min_stock_data_date)
         await bot.change_presence(status=discord.Status.online, activity=discord.Game("Waiting"))
     except Exception as e:
         await ctx.send(f"An error occurred while processing {stock}: {e}")
         print(f"Error processing {stock}: {e}")
+
 
 
